5a9b300567373385491c979e4468bf68a24058bb
GenzBIT | genznodes <94878333+Genz22@users.noreply.github.com>
2022-10-19 11:33:51 +0700

    Commit Permaswap client

---
+++ Permaswap Client/run.sh
@@ -0,0 +1,6 @@
+export PERMA_WS="wss://router0-dev.permaswap.network/wslp"
+export PERMA_HTTP="https://router0-dev.permaswap.network"
+export LP_CONFIG="./lp_config.json"
+export ECC_PRIVATE=""
+#export AR_WALLET="ar_wallet.json"
+./lp
